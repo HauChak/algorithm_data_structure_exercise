2e97cd44d317ed6cd3060431c1b9596c7821e507
恒泽 詹 <lyzed109@163.com>
2019-03-16 17:09:50 +0800

    +1

--- algorithm_data_structure/src_code/SaleStockTimeThree.h
+++ algorithm_data_structure/src_code/SaleStockTimeThree.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 class SaleStockTimeThree
@@ -7,7 +8,19 @@ class SaleStockTimeThree
 public:
 	SaleStockTimeThree();
 	int maxProfit(vector<int>& prices) {
-
+		if (prices.size() == 0) return 0;
+		int sz = prices.size();
+		vector<vector<int>>local_dp(sz+1, vector<int>(3, 0)), global_dp(sz+1, vector<int>(3, 0));
+		for (int i = 1; i < sz; ++i)
+		{
+			for (int j = 1; j <= 2; ++j)
+			{
+				int diff = prices[i] - prices[i - 1];
+				local_dp[i][j] = max(local_dp[i - 1][j], global_dp[i - 1][j - 1]) + diff;;
+				global_dp[i][j] = max(global_dp[i - 1][j], local_dp[i][j]);
+			}
+		}
+		return global_dp[sz-1][2];
 	}
 	~SaleStockTimeThree();
 };
