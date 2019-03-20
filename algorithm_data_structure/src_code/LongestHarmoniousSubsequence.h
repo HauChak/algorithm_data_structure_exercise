97e2bce75faeeed883e751864e0940108329204c
恒泽 詹 <lyzed109@163.com>
2019-03-21 07:48:11 +0800

    3-21

---
+++ algorithm_data_structure/src_code/LongestHarmoniousSubsequence.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <vector>
+#include <algorithm>
+#include<string>
+#include <map>
+using namespace std;
+
+class LongestHarmoniousSubsequence
+{
+public:
+	int findLHS(vector<int>& nums) {
+		if (nums.empty()) return 0;
+		int res = 0;
+		map<int, int> m;
+		for (int num : nums) ++m[num];
+		for (auto it = next(m.begin()); it != m.end(); ++it) {
+			auto pre = prev(it);
+			if (it->first == pre->first + 1) {
+				res = max(res, it->second + pre->second);
+			}
+		}
+		return res;
+	}
+};
