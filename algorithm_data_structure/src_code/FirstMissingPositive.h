e5644bee9b4852c9105e9653808c76137716fbc2
lyzed109 <lyzed109@163.com>
2019-03-28 22:49:10 +0800

    328

--- algorithm_data_structure/src_code/FirstMissingPositive.h
+++ algorithm_data_structure/src_code/FirstMissingPositive.h
@@ -8,15 +8,20 @@ class FirstMissingPositive
 public:
 	FirstMissingPositive() {};
 	int firstMissingPositive(vector<int>& nums) {
-		int res = 1;
-		for (int i = 0; i < nums.size(); ++i)
+		int n = nums.size();
+		for (int i = 0; i < n; ++i)
 		{
-			if (nums[i] <= 0)continue;
-			else if (nums[i] < res)
-				res = res+1;
-			else
-				res = res;
+			while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i])
+			{
+				swap(nums[i], nums[nums[i] - 1]);
+			}
 		}
+		for (int i=0;i<n;++i)
+		{
+			if (nums[i] != i + 1)
+				return i + 1;
+		}
+		return n + 1;
 	}
 	~FirstMissingPositive() {};
 
