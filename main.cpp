4ec7b0765468bd68688e3df2783f3901e57b4b12
hzzhan <im.hzzhan@gmail.com>
2019-10-01 21:34:50 +0800

    gdb debug support

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include "algorithm_data_structure/src_code/pch.h"
 #include <vector>
+#include<unordered_map>
+#include<string>
 #define Max(a,b) a*b
 using namespace std;
 
@@ -9,14 +11,27 @@ void test(ListNode* given)
 	given= tmp;
 }
 
+struct Point
+{
+	int x;
+	int y;
+};
+
 int main()
 {
+	Point p={1,2};
 	vector<int>src = { 1,1,1,0 };
 	int a[]={1,2,3};
+	string ss ="hello world";
+	string sub_str = ss.substr(0,5);
+	string::iterator it =ss.begin();
+	for(;it!=ss.end();++it)
+		cout<<*it<<endl;
 	cout<<a<<endl;
 	cout<<"hello world"<<endl;
 	JumpGame demo;
 	bool res = demo.canJump(src);
-	system("pause");
+	cout<<res<<endl;
+	//getchar();
 	return 0;
 }
